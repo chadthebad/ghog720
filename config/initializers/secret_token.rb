591e28169fe31690e3e03f19866984f15010edc6
Chad Dibble <chad@chadthebad.com>
2012-07-20 17:05:20 -0700

    initial commit.  created album model and controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghog720::Application.config.secret_token = '9f6d3b08e4d9a7e022b8a16f80440f9d0521407a5cc55c604eeaba252c2ad71f2a1124f54d7f63f0ea1146b4bd78f3a2e9a7b065b7ad007d6fee16427a18d58e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ghog720::Application.config.session_store :cookie_store, key: '_ghog720_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ghog720::Application.config.session_store :active_record_store
